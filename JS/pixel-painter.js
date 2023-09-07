ae38788b6ec31e40bf897de5a71c1f88cade5d0c
Jangmyun <cndlsrb2739@gmail.com>
2023-09-07 11:41:12 +0900

    픽셀 element 만들고 각각에 down 이벤트리스너 적용

--- JS/pixel-painter.js
+++ JS/pixel-painter.js
@@ -6,6 +6,9 @@ let eraseBtn = document.getElementById('erase-btn');
 let pixelResolution = document.getElementById('resolution-range');
 let resolutionValue = document.getElementById('resolution-value');
 
+//초기 픽셀값 초기화
+resolutionValue.innerText = pixelResolution.value;
+
 // 이벤트 객체 - 터치 스크린 디바이스에서는 터치로 그릴 수 있도록
 let events = {
     mouse:{
@@ -32,6 +35,33 @@ let erase = false;
 
 //pixel 생성 - range input값이 변경될 때마다
 pixelResolution.addEventListener('change', function(e){
+    let count = 0;
+    let resolution = e.target.value;
+    // input값 변경되면 기존 픽셀 컨테이너 안에 있던 픽셀 다 없애기
+    pixelContainer.innerHTML = "";
+    //range input 옆에 값 변경되도록
     resolutionValue.innerText = e.target.value;
+    
+    for(let i=0; i<resolution; i++){
+        for(let j=0; j<resolution; j++){
+            let pixel = document.createElement("div");
+            pixel.classList('pixel');
+            pixel.setAttribute('id', `pixel-${count++}`);
+
+            // 마우스 누르거나 터치 시작한 시점
+            pixel.addEventListener(events[deviceType].down, function(e){
+                draw = true;
+
+                if(erase) {
+                    pixel.style.backgroundColor='white';
+                }else {
+                    pixel.style.background = colorInput.value;
+                }
+            });
+
+        }
+    }
+
+    
 });
 
