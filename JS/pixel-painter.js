2ba28485ffa2e6c7d7f1e84d9c38498c6df59bc8
Jangmyun <cndlsrb2739@gmail.com>
2023-09-08 00:49:34 +0900

    픽셀 이벤트 리스너 함수 동작 안하는 문제 수정, pixel에 이벤트리스너 적용 전에 deviceType 변수 초기화 안되는 문제 수정

--- JS/pixel-painter.js
+++ JS/pixel-painter.js
@@ -6,24 +6,6 @@ let eraseBtn = document.getElementById('erase-btn');
 let pixelResolution = document.getElementById('resolution-range');
 let resolutionValue = document.getElementById('resolution-value');
 
-// color input 바뀔때마다 바뀐 값 저장
-let pixelColor = 'white';
-colorInput.addEventListener('change', function(e){
-    pixelColor = e.target.value;
-});
-
-
-
-// grid resolution 바뀔때마다 바뀐 값 저장
-let resolution =24;
-pixelResolution.addEventListener('change', function(e){
-    resolution = e.target.value;
-    resolutionValue.innerText = resolution;
-    newPixels(resolution);
-});
-//초기 픽셀값 초기화
-resolutionValue.innerText = resolution;
-
 // 이벤트 객체 - deviceType에 따라 다른 이벤트 적용 (마우스, 터치)
 let events = {
     mouse:{
@@ -52,11 +34,39 @@ function isTouchDevice(){   //터치 디바이스 체크하는 함수
 }
 isTouchDevice();
 
+// color input 바뀔때마다 바뀐 값 저장
+let pixelColor = '#000';
+colorInput.addEventListener('change', function(e){
+    pixelColor = e.target.value;
+});
+
+// grid resolution 바뀔때마다 바뀐 값 저장
+let resolution =24;
+pixelResolution.addEventListener('change', function(e){
+    resolution = e.target.value;
+    resolutionValue.innerText = resolution;
+    newPixels(resolution);
+});
+//초기 픽셀값 초기화
+resolutionValue.innerText = resolution;
+newPixels(resolution);
+
+// 마우스 클릭한 채로 드래그하면 그려지고 지워지도록 boolean 값
 let draw = false;
 let erase = false;
 
+eraseBtn.addEventListener('onclick', function(e){
+    erase = true;
+    console.log(erase);
+});
+clearBtn.addEventListener('onclick', function(e){
+    newPixels(resolution);
+});
+
+
 // pixel 생성 함수 
 function newPixels(resol) {
+    // 1씩 증가하며 픽셀마다 고유의 id값을 가지도록 함
     let idCounter = 0;
     pixelContainer.innerHTML = "";
     for(let i=0; i<resol; i++){
@@ -64,68 +74,35 @@ function newPixels(resol) {
             pixelContainer.innerHTML += `
             <div class="pixel" id="pixel-${idCounter}" style="width: ${100/resol}%; padding-bottom: ${100/resol}%"></div>
             `
-            idCounter ++ ;
+            idCounter ++ ; 
         }
     }
+    pixelEventListener();
 }
 
 //생성된 pixel 에 이벤트리스너 적용
 function pixelEventListener(){
     let pixels = document.querySelectorAll('.pixel');
     pixels.forEach(function(pixel){
+        // 마우스를 누르거나 화면을 터치하면 draw = true 
         pixel.addEventListener(events[deviceType].down, function(e){
             draw = true;
         });
+        // 마우스를 움직이거나 터치한채로 움직이면 그려지도록
+        pixel.addEventListener('mouseover', function(e){
+            if( draw && !erase){
+                e.target.style.backgroundColor = pixelColor;
+            }else if( draw && erase){
+                e.target.style.backgroundColor = '#FFF';
+            }
+        });
+        pixel.addEventListener(events[deviceType].up , function(e){
+            draw = false;
+        });
     });
 }
+pixelEventListener();
 
-
-
-// //pixel 생성 - range input값이 변경될 때마다
-// pixelResolution.addEventListener('change', function(e){
-//     let count = 0;
-//     let resolution = e.target.value;
-//     // input값 변경되면 기존 픽셀 컨테이너 안에 있던 픽셀 다 없애기
-//     pixelContainer.innerHTML = "";
-//     //range input 옆에 값 변경되도록
-//     resolutionValue.innerText = e.target.value;
-    
-//     for(let i=0; i<resolution; i++){
-//         for(let j=0; j<resolution; j++){
-//             let pixel = document.createElement("div");
-//             pixel.classList.add('pixel');
-//             pixel.setAttribute('id', `pixel-${count++}`);
-//             pixel.style.paddingBottom = `${100/resolution}%`;
-//             pixel.style.width = `${100/resolution}%`;
-//             pixel.style.backgroundColor = 'white';
-
-//             // 마우스 누르거나 터치 시작한 시점
-//             pixel.addEventListener(events[touchDevise].down, function(e){
-//                 draw = true;
-
-//                 if(erase) {
-//                     pixel.style.backgroundColor='white';
-//                 }else {
-//                     pixel.style.background = colorInput.value;
-//                 }
-//             });
-//             // 마우스 움직이거나 터치한 상태로 움직일 때
-//             pixel.addEventListener(events[touchDevise].move, function(e){
-//                 let pixelId = document.elementsFromPoint(
-//                     isTouchDevise() ? e.clientX : e.touches[0].clientX,
-//                     isTouchDevise() ? e.clientY : e.touches[0].clientY
-//                 ).id;
-//                 checker(elementId);
-//             });
-//             // 마우스 클릭 떼거나 터치 끝나는 시점
-//             pixel.addEventListener(events[touchDevise].up, function(e){
-//                 draw = false;
-//             });
-//             pixelContainer.innerHTML+= pixel.outerHTML;
-//             console.log
-//         }
-//     }
-// });
 
 function checker(elementId){
     let pixels = document.querySelectorAll(".pixel");
