b9c76d4976532e7c2979d9128da1e73923adefc7
Jangmyun <cndlsrb2739@gmail.com>
2023-10-15 17:15:01 +0900

    css 디렉토리 수정

--- my-porfolio-webpage/src/App.js
+++ my-porfolio-webpage/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import './CSS/App.css';
+import FrontBack from './Components/FrontBack';
+import Header from './Components/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <FrontBack></FrontBack>
+      <div style={{height: '1500px'}}></div>
     </div>
   );
 }
