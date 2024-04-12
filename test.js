bdeff9b2d3d70f021816887fdee5507b96345cf8
Jangmyun <cndlsrb2739@gmail.com>
2024-04-12 13:00:45 +0900

    test

---
+++ test.js
@@ -0,0 +1,13 @@
+// const obj = {
+//   name: "melon",
+//   weight: 1000,
+//   price: 1000,
+// };
+
+// const arr = ["abc", "cdg"];
+
+// const keys = Object.keys(obj);
+
+// for (let key of keys) {
+//   console.log(key);
+// }
